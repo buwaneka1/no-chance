5a224034932c1d1e9a9bbdbfefb68c17d0bb912c
Nadun Malinda <nadun.m@eyepax.com>
2019-02-24 03:48:05 +0530

    react-33

--- react/src/components/RantList.js
+++ react/src/components/RantList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Rant from './Rant';
 import '../styles/rantlist.css';
 
 class RantList extends Component {
@@ -8,29 +9,7 @@ class RantList extends Component {
             <div className="post-list">
                     
                 <Link to="/rant/1">
-                    <article className="post">
-                        <div className="post__inner">
-                            <div className="score">
-                                <div className="score__up layout--center">++</div>
-                                <div className="score__board layout--center">100</div>
-                                <div className="score__down layout--center">--</div>
-                                </div>
-                            <div className="post__body">
-                                    Hello World... </div>
-                            </div>
-                        <div className="post__footer">
-                        <div className="post__time">2m ago</div>
-                        <div className="post__comments">
-                            <svg className="icon" viewBox="0 0 31 32">
-                                    <path d="M24.732 24.371v7.629l-7.267-7.267h-8.808c-4.781 
-                                    0-8.657-3.875-8.657-8.657v-7.42c0-4.781 3.876-8.657 
-                                    8.657-8.657h13.604c4.781 0 8.657 3.875 8.657 8.657v7.42c0 
-                                    3.922-2.61 7.23-6.186 8.294z"></path>
-                                </svg>
-                                23
-                            </div>
-                        </div>
-                    </article>
+                    <Rant />
                 </Link>
                     
                 <div className="rant__add" title="Add Rant">+</div>        
