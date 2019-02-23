a719350fd110b4d9452a83b4c75e71e532ea4cd7
buwaneka.kalansuriya <buwaneka.kalansuriya@eyepax.com>
2019-02-23 13:15:35 +0530

    angular-6

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from './header/header.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, LoaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
