626295adea634ddfa11d6417d407b2bbce064d87
Nadun Malinda <nadun.m@eyepax.com>
2019-02-24 07:10:44 +0530

    react-40

--- react/src/components/Alert.js
+++ react/src/components/Alert.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 class Alert extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        const title = this.props.title;
+        const description = this.props.description;
+
         return (
             <div className="popup popup--open">
                 <div className="popup__header">
@@ -14,10 +21,10 @@ class Alert extends Component {
 
                         <div className="form">
                             <div className="form__title">
-                                <span className="highlight">#</span>OOPS!
+                                <span className="highlight">#</span>{title}
                             </div>
                             <div className="form__description">
-                                You can not vote on your own post :)
+                                {description}
                             </div>
                             <form name="alert">
                                 <div className="alert">
